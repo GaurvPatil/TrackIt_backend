1fd35c276289237d34728e73e9f686ae579fcf71
GaurvPatil <gaurvpatil17@gmail.com>
2025-03-08 12:53:13 +0530

    Refactor <Monitoring> : refactored express monitoring functionality.

--- src/server-monitoring/monitorServerConnection.ts
+++ src/server-monitoring/monitorServerConnection.ts
@@ -2,27 +2,29 @@ import expressApp from "../express-server-connection/expressApp";
 import { API_URL } from "../utils/constants/constants";
 import dotenv from "dotenv";
 dotenv.config();
+import { Express } from "express";
 
-
-const monitorServer = async () => {
+const monitorServer = async (app: Express) => {
   const interval =
-    parseInt(process.env.SERVER_CONNECTION_MONITORING_INTERVAL_MS as string, 10) ||
-    30 * 60 * 1000; // 30 minutes default
- 
+    parseInt(
+      process.env.SERVER_CONNECTION_MONITORING_INTERVAL_MS as string,
+      10
+    ) || 30 * 60 * 1000; // 30 minutes default
+
   setInterval(async () => {
     try {
       console.log("Checking server health...");
 
-      const response = await fetch(`${API_URL}/health`);
-      if (response.status !== 200) {
+      const restAPiresponse = await fetch(`${API_URL}/health`);
+      if (restAPiresponse.status !== 200) {
         throw new Error("Server health check failed.");
       }
-
       console.log("Server is healthy.");
+   
     } catch (error) {
       console.error("Server health check failed. Restarting server...", error);
       try {
-        await expressApp(); // Restart server
+        await expressApp(app); // Restart server
       } catch (restartError) {
         console.error("Failed to restart the server:", restartError);
       }
